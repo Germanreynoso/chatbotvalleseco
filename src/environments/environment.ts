fb39bc0518c4419670e6c6c39025d0fa272c2e79
germanreynoso <germanreynoso94@gmail.com>
2025-08-27 12:00:11 -0300

    cambio webhookurl a produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlBase: 'http://api.dev.chatbot.valleseco.mmi-e.com:9080',
-  webhookUrl: 'https://n8n.icc-e.org/webhook-test/d565fa7c-3cf0-4f39-ab9b-1ca66cb75010',
+  webhookUrl: 'https://n8n.icc-e.org/webhook/d565fa7c-3cf0-4f39-ab9b-1ca66cb75010',
 };
