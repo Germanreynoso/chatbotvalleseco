cc112f4dbf0e2fbe4758c7bed2788468dfee2661
germanreynoso <germanreynoso94@gmail.com>
2025-08-28 23:09:17 -0300

    roguemos que ahora ande

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlBase: 'http://api.dev.chatbot.valleseco.mmi-e.com:9080',
-  webhookUrl: 'https://n8n.icc-e.org/webhook-test/8d9e1372-67e9-49f4-baa3-4a282f575e16',
+  webhookUrl: 'https://n8n.icc-e.org/webhook/8d9e1372-67e9-49f4-baa3-4a282f575e16',
 };
